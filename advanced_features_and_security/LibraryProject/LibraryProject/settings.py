4827160a4db8a60862b6b3aa8fe2663026b46d29
ekeocha <ekeochacr@gmail.com>
2025-08-13 09:32:33 +0100

    Secure proxy added

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -55,6 +55,9 @@ MIDDLEWARE = [
     'csp.middleware.CSPMiddleware',
 ]
 
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 SECURE_SSL_REDIRECT = True
 SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True   # include subdomains

8cac36b3c5883dbd906d275dd5a13536d1f503ea
ekeocha <ekeochacr@gmail.com>
2025-08-19 10:50:03 +0100

    Serialized for CRUD

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,11 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
-# api/views.py
-from rest_framework import generics
+from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(generics.ListAPIView):
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer

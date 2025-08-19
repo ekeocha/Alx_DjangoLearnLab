75d31a78ebd9a5a8b7e9a4b232486b1b18e04b2b
ekeocha <ekeochacr@gmail.com>
2025-08-19 07:25:40 +0100

    Created API For Book Model

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
-    
+    published_date = models.DateField(null= True,blank= True)
+    isbn = models.CharField(max_length=13, unique=True, default="0000000000000")
+
+    def __str__(self):
+        return self.title

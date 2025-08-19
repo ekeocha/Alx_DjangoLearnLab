3b0bcb33f7049c70282770df35614ac03c07ac93
ekeocha <ekeochacr@gmail.com>
2025-08-19 05:21:05 +0100

    New API Project and App created

--- Thex/thexora_app/urls.py
+++ Thex/thexora_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import BookListCreateAPIView
+from . import views
 
 urlpatterns = [
-    path("api/books", BookListCreateAPIView.as_view(), name="book_list_create"),
+    path("books", views.BookListCreateAPIView.as_view(), name="book_list_create"),
 ]

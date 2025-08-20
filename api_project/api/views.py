7d7ccfa8b97faeefe3fb7eb6e92020082a8bab6f
ekeocha <ekeochacr@gmail.com>
2025-08-20 13:39:17 +0100

    added authenticated

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]

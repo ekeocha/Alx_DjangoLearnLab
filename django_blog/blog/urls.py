f2d7be60bcd733bf4b21c8e1c9b41c1e49246908
ekeocha <ekeochacr@gmail.com>
2025-09-07 21:53:55 +0100

    update app url

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,9 +14,9 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
 
-    path("posts/",                    PostListView.as_view(),   name="post_list"),
-    path("posts/new/",                PostCreateView.as_view(), name="post_create"),
-    path("posts/<int:pk>/",           PostDetailView.as_view(), name="post_detail"),
-    path("posts/<int:pk>/edit/",      PostUpdateView.as_view(), name="post_update"),
-    path("posts/<int:pk>/delete/",    PostDeleteView.as_view(), name="post_delete"),
+    path("posts/", PostListView.as_view(), name="post_list"),
+    path("post/new/", PostCreateView.as_view(), name="post_create"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post_update"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
 ]
